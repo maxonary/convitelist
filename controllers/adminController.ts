f2427c7d7e3c4714e780837cf04982856f1aa2e0
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2023-05-07 14:09:00 +0200

    Feat: Rcon controlls

--- controllers/adminController.ts
+++ controllers/adminController.ts
@@ -35,33 +35,53 @@ export async function adminLogin(req: AuthenticatedRequest, res: Response) {
     const token = jwt.sign({ adminId: admin.id }, jwtSecret, { expiresIn: '1h' });
     res.status(200).json({ token });
   } catch (error) {
-    res.status(500).json({ error: (error as Error).message });
+    res.status(500).json({ error: error.message });
   }
 }
 
 export async function approveUser(req: AuthenticatedRequest, res: Response) {
   try {
     const userId = parseInt(req.params.userId, 10);
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+
+    if (!user) {
+      res.status(404).json({ error: 'User not found' });
+      return;
+    }
+
+    await sendRconCommand(`whitelist add ${user.minecraftUsername}`);
+
     const updatedUser = await prisma.user.update({
       where: { id: userId },
       data: { approved: true },
     });
+
     res.status(200).json(updatedUser);
   } catch (error) {
-    res.status(400).json({ error: (error as Error).message });
+    res.status(400).json({ error: error.message });
   }
 }
 
 export async function rejectUser(req: AuthenticatedRequest, res: Response) {
   try {
     const userId = parseInt(req.params.userId, 10);
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+
+    if (!user) {
+      res.status(404).json({ error: 'User not found' });
+      return;
+    }
+
+    await sendRconCommand(`whitelist remove ${user.minecraftUsername}`);
+
     const updatedUser = await prisma.user.update({
       where: { id: userId },
       data: { approved: false },
     });
+
     res.status(200).json(updatedUser);
   } catch (error) {
-    res.status(400).json({ error: (error as Error).message });
+    res.status(400).json({ error: error.message });
   }
 }
 
