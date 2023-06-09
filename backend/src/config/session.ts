7cebc2037519367439dfb0a3678d8c1b9a4762fc
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2023-06-09 15:36:09 +0200

    Feat: Session

---
+++ backend/src/config/session.ts
@@ -0,0 +1,18 @@
+import session from "express-session";
+import { Express } from "express";
+import dotenv from "dotenv";
+
+export const configureSession = (app: Express) => {
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || "fallback-secret-key",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        secure: false, // Set it to 'true' if using HTTPS
+        httpOnly: true,
+        maxAge: 3600000, // Session expiration time (in milliseconds)
+      },
+    })
+  );
+};
