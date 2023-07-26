a8ecf7f6a031ee9786adf43ce29a78d41f70d66f
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2023-07-26 14:47:56 +0200

    Feat: Use .env now

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   headers: {
     'Content-Type': 'application/json',
   },
---
+++ frontend/src/webpack.config.ts
@@ -0,0 +1,12 @@
+/// <reference types="node" />
+console.log('Webpack config loaded');
+
+export default {
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "crypto": require.resolve("crypto-browserify")
+    }
+  }
+};
