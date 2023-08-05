1064e792a19b8d98e69d4e3d175cce535d437ef7
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2023-08-05 17:23:38 +0200

    Hotfix: Back navigate if previousPath not current

--- frontend/src/utils/navigation.ts
+++ frontend/src/utils/navigation.ts
@@ -1,9 +1,18 @@
 import { NavigateFunction } from 'react-router-dom';
 
 export const navigateBackOrHome = (navigate: NavigateFunction) => {
+  // Get the current location from the window's location object
+  const currentPath = window.location.pathname;
+
+  // Check if there is a previous page in the history stack
   if (window.history.length > 1) {
-    navigate(-1); // Go back to previous page
+    const previousPath = window.location.href; // Get the URL of the previous page
+    if (previousPath !== currentPath) {
+      navigate(-1); // Go back to previous page
+    } else {
+      navigate('/'); // Redirect to the homepage if previous page is the same as current page
+    }
   } else {
-    navigate('/');
+    navigate('/'); // Redirect to the homepage if there's no previous page
   }
 };
