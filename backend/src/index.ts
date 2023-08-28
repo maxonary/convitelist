7cd715e85c5d55aeed1807d8c64184ce6d90b8a9
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2023-08-28 11:35:31 +0200

    Feat: Add authMiddleware to server routing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import passport from "./config/passport";
 import { configureSession } from "./config/session";
+import { authMiddleware } from "./middleware/authMiddleware";
 import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
@@ -35,10 +36,10 @@ app.get('/api', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Minecraft Registration API!' });
 });
 
-app.use('/api/admin', adminRoutes);
+app.use('/api/admin', authMiddleware, adminRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/invitation', invitationRoutes);
-app.use('/api/user', userRoutes);
+app.use('/api/invitation', authMiddleware, invitationRoutes);
+app.use('/api/user', authMiddleware, userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
