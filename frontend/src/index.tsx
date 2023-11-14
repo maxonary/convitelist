5ddb520f2f27154b176ff73893d8e67fc106a98c
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2023-11-14 11:32:24 +0100

    Refactor: Remove unused code

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -10,21 +10,9 @@ import { AuthProvider } from "react-auth-kit";
 import { Provider as StyletronProvider } from "styletron-react";
 import { Client as Styletron } from "styletron-engine-atomic";
 
-import {
-  BaseProvider,
-  styled,
-  DarkTheme,
-} from "baseui";
 
 const engine = new Styletron();
 
-const Centered = styled("div", {
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  height: "100%",
-  width: "100%",
-});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -32,25 +20,16 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <StyletronProvider value={engine}>
-      <BaseProvider
-        theme={DarkTheme}
-        overrides={{
-          AppContainer: { style: { width: "100%", height: "100%" } },
-        }}
-      >
         <AuthProvider
           authType={"cookie"}
           authName={"__auth__"}
           cookieDomain={window.location.hostname}
           cookieSecure={window.location.protocol === "https:"}
         >
-          <Centered>
             <BrowserRouter>
               <App />
             </BrowserRouter>
-          </Centered>
         </AuthProvider>
-      </BaseProvider>
     </StyletronProvider>
   </React.StrictMode>
 );
