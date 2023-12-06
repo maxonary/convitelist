a6b974a1198516177f38ac1bc6ea21c36f1b807e
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2023-12-07 00:43:57 +0100

    Add invitationHelper test

---
+++ backend/tests/helpers/invitationHelper.test.ts
@@ -0,0 +1,34 @@
+import generateInvitationCode from '../../src/helpers/invitationHelper';
+import { describe, beforeEach, it, expect, vi } from 'vitest';
+import prismaMock from '../../src/lib/__mocks__/prisma';
+import crypto from 'crypto';
+
+vi.mock('lib/prisma')
+vi.mock('crypto', () => ({
+    default: vi.fn(() => ({
+        randomBytes: vi.fn(() => 'mocked-code')
+    }))
+})),
+
+
+describe('invitationHelper', () => {
+    beforeEach(() => {
+        vi.restoreAllMocks()
+    });
+    describe('generateInvitationCode', () => {
+        it('should generate a unique invitation code and store it in the database', async () => {
+            const code = await generateInvitationCode()
+
+            expect(code).toBe('mocked-code');
+            expect(prismaMock.invitationCode.create).toHaveBeenCalledWith({
+            data: {
+                code: 'mocked-code',
+                used: false,
+            },
+            })
+            expect(prismaMock.$disconnect).toHaveBeenCalled()
+            expect(crypto.randomBytes).toHaveBeenCalledWith(16)
+            expect(crypto).toHaveBeenCalledTimes(1)
+        })
+    })
+})
