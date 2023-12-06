c3153665a6d481d5b04b83f958f4dc78470dc1ff
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2023-12-06 22:07:06 +0100

    Remove file aliases that dont need to be tested

--- backend/vitest.config.unit.ts
+++ backend/vitest.config.unit.ts
@@ -6,15 +6,11 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-        config: 'src/config',
         controllers: 'src/controllers',
         helpers: 'src/helpers',
         middleware: 'src/middlewares',
-        routes: 'src/routes',
         scripts: 'src/scripts',
-        types: 'src/types',
         utils: 'src/utils',
-        index: 'src/index.ts'
     }
   }
 })
