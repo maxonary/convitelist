3cae1a22b6bc1de736a6f9f50b15f955bb602625
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2023-12-07 07:52:01 +0100

    Update vitest.config.unit.ts to exclude src/tests from test include

--- backend/vitest.config.unit.ts
+++ backend/vitest.config.unit.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['src/**/*.test.ts']
+    include: [
+        'src/**/*.test.ts',
+        '!src/tests'
+      ]
   },
   resolve: {
     alias: {
