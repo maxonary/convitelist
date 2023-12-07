1939ac2ba60e9930ebb09ac99a3f3cc3b0ec5071
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2023-12-07 08:02:02 +0100

    Add database readiness check in run-integration.sh script

--- backend/scripts/run-integration.sh
+++ backend/scripts/run-integration.sh
@@ -1,5 +1,9 @@
+#From https://www.prisma.io/blog/testing-series-3-aBUyF8nxAn
+
 #!/usr/bin/env bash
 # scripts/run-integration.sh
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 source $DIR/setenv.sh
+echo '🟡 - Waiting for database to be ready...'
+$DIR/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
