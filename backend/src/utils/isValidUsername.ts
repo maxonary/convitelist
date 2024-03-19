920a4aa7c4b02d953407aa28dc9404e00489349d
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2024-03-19 16:12:50 +0100

    Refactor isValidUsername Extract Regular Expressions

--- backend/src/utils/isValidUsername.ts
+++ backend/src/utils/isValidUsername.ts
@@ -1,4 +1,7 @@
+const JAVA_EDITION_USERNAME_PATTERN = /^\w{3,16}$/;
+const OTHER_EDITION_USERNAME_PATTERN = /^\w[\w ]{1,14}\w$/;
+
 export const isValidUsername = (username: string, gameType: string) => {
-  const usernamePattern = gameType === 'Java Edition' ? /^\w{3,16}$/ : /^\w[\w ]{1,14}\w$/;
+  const usernamePattern = gameType === 'Java Edition' ? JAVA_EDITION_USERNAME_PATTERN : OTHER_EDITION_USERNAME_PATTERN;
   return usernamePattern.test(username);
 }
