a01bc29ce00919edccee0df256f8783178e07759
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2024-03-19 16:15:28 +0100

    Refactor return type to isValidUsername function

--- backend/src/utils/isValidUsername.ts
+++ backend/src/utils/isValidUsername.ts
@@ -1,7 +1,7 @@
 const JAVA_EDITION_USERNAME_PATTERN = /^\w{3,16}$/;
 const OTHER_EDITION_USERNAME_PATTERN = /^\w[\w ]{1,14}\w$/;
 
-export const isValidUsername = (username: string, gameType: string) => {
+export const isValidUsername = (username: string, gameType: string): boolean => {
   const usernamePattern = gameType === 'Java Edition' ? JAVA_EDITION_USERNAME_PATTERN : OTHER_EDITION_USERNAME_PATTERN;
   return usernamePattern.test(username);
 }
