cbd356af7bf30a3a71cad766940e106f4b668889
Maximilian Arnold <62939182+maxonary@users.noreply.github.com>
2024-03-19 16:10:07 +0100

    Refactor isValidUsername regex

--- backend/src/utils/isValidUsername.ts
+++ backend/src/utils/isValidUsername.ts
@@ -1,4 +1,4 @@
 export const isValidUsername = (username: string, gameType: string) => {
-  const regex = gameType === 'Java Edition' ? /^[a-zA-Z0-9_]{3,16}$/ : /^[a-zA-Z0-9_][a-zA-Z0-9_ ]{1,14}[a-zA-Z0-9_]$/;
+  const regex = gameType === 'Java Edition' ? /^\w{3,16}$/ : /^\w[\w ]{1,14}\w$/;
   return regex.test(username);
 }
