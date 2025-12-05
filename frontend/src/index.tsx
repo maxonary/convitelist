8b221e4e56972c75ae6f47b9e6ae4e4699fbfebd
Maximilian <maximilianmarnold@gmail.com>
2025-12-05 22:51:26 +0100

    feat: Add Vercel Analytics integration to frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 import { AuthProvider } from "react-auth-kit";
 
@@ -29,6 +30,7 @@ root.render(
             </BrowserRouter>
         </AuthProvider>
     </StyletronProvider>
+    <Analytics />
   </React.StrictMode>
 );
 
